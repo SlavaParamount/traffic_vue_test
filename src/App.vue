d0df3ec87026b88dd34bfb6ad78a18a50b42fa6b
Vyacheslav <viparamount@gmail.com>
2019-09-13 11:31:32 +0300

    added timer

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,12 @@
     <div id="app">
       <div class="content-wrapper">
         <h1>Traffic lights</h1>
+        <p>{{ link }}</p>
+        <p style="font-size: 36px;">{{timeout}}</p>
         <div id="traffic-light">
-          <router-link to ="#">aaaa</router-link>
-          <input type="radio" name="traffic-light-color" id="color1" value="color1" />
-          <input type="radio" name="traffic-light-color" id="color2" value="color2"/>
-          <input type="radio" name="traffic-light-color" id="color3" value="colo3" />
+          <router-link to="./red" class='color color-red' id="color1" value="color1" v-on:click="changeTo('red')"/></router-link>
+          <router-link to = "./yellow" class='color color-yellow' id="color2" v-on:click="changeTo('yellow')" value="color2"/><span></span></router-link>
+          <router-link to="./green" class='color color-green' id="color3" value="colors3" v-on:click="changeTo('green')" /><span></span></router-link>
         </div>
       </div>
     </div>
@@ -15,13 +16,65 @@
 </template>
 
 <script>
-export default {
+export default { //это должно быть здесь? почему не работает v-for?
   name: 'app',
-  data () {
+  data: function() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      timeout: 5,
+      interval: undefined
     }
-  }
+  },
+  computed: {
+    link: function() {
+      return this.$route.path;
+    }
+  },
+  methods: {
+    changeTo(color) {
+      clearInterval(this.interval);
+      console.log(color);
+      switch(color) {
+        case "red":
+        this.timeout=10;
+        this.$router.push("/red");
+        break;
+        case "yellow":
+        this.timeout=3;
+        this.$router.push("/yellow");
+        break;
+        case "green":
+        this.timeout=15;
+        this.$router.push("/green");
+      }
+      this.interval = this.setTimer;
+      this.interval();
+    },
+    nextPath() {
+      switch(this.$route.path) {
+        case "/red":
+          this.changeTo("yellow");
+          break;
+        case "/yellow":
+          this.changeTo("green");
+          break;
+        default:
+          this.changeTo("red");
+          break;
+      }
+    },
+    setTimer() {
+    var that = this;
+     this.interval =  setInterval(function(){
+      that.timeout-=1;
+      if (that.timeout==0) {
+      that.nextPath();
+     }
+    }, 1000);
+    }
+  },
+  mounted: function(){
+    this.nextPath();
+  },
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/red' },
+  { path: '/yellow' },
+  { path: '/green' }
+]
+
+const router = new VueRouter({
+	routes
+})
 
 new Vue({
+	router,
   el: '#app',
   render: h => h(App)
 })
